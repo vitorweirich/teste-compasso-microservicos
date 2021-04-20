48415cbd1c28c184cca42504717072b47de90fe5
Vitor Mateus Weirich <vitor.weirich@pampa.compasso>
2021-04-20 09:39:07 -0300

    Projeto finalizado

--- ct-cidade/src/main/java/br/com/compasso/ctcidade/dto/CidadeDto.java
+++ ct-cidade/src/main/java/br/com/compasso/ctcidade/dto/CidadeDto.java
@@ -7,19 +7,19 @@ import br.com.compasso.ctcidade.model.Cidade;
 
 public class CidadeDto {
 
-	//private Long id;
+	private Long id;
 	private String nome;
 	private String estado;
 
 	public CidadeDto(Cidade cidade) {
-		//this.id = cidade.getId();
+		this.id = cidade.getId();
 		this.nome = cidade.getNome();
 		this.estado = cidade.getEstado();
 	}
 
-//	public Long getId() {
-//		return id;
-//	}
+	public Long getId() {
+		return id;
+	}
 
 	public String getNome() {
 		return nome;
